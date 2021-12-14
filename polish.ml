a8fa9692d92213000d9f929a832b72b76fa73533
KELLER dylan <dylan.b.keller@gmail.com>
2021-12-14 19:35:10 +0100

    ajout de description des commandes du programme lors d'un run erroné (indirectement, ajout d'un --help)

--- polish.ml
+++ polish.ml
@@ -6,7 +6,9 @@ open Lib.EvalPolish
 
 let usage () =
   print_string "Polish : analyse statique d'un mini-langage\n";
-  print_string "usage: à documenter (TODO)\n"
+  print_string "usage:\n"
+  print_string "  --eval <fichier Polish> : évalue le programme Polish donné"
+  print_string "  --reprint <fichier Polish> : réaffiche le programme Polish donné\n"
 
 let main () =
   match Sys.argv with
