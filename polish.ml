7c356f685109566b64b58b59a37764a13a634d2b
KELLER dylan <dylan.b.keller@gmail.com>
2021-12-16 11:51:40 +0100

    rajout d'un ; oublié

--- polish.ml
+++ polish.ml
@@ -6,15 +6,17 @@ open Lib.EvalPolish
 
 let usage () =
   print_string "Polish : analyse statique d'un mini-langage\n";
-  print_string "usage:\n"
-  print_string "  --eval <fichier Polish> : évalue le programme Polish donné"
-  print_string "  --reprint <fichier Polish> : réaffiche le programme Polish donné\n"
+  print_string "usage:\n";
+  print_string "  --eval <fichier Polish> : évalue le programme Polish donné";
+  print_string "  --reprint <fichier Polish> : réaffiche le programme Polish donné\n";
+;;
 
 let main () =
   match Sys.argv with
   | [|_;"--reprint";file|] -> print_polish (read_polish file)
   | [|_;"--eval";file|] -> eval_polish (read_polish file)
   | _ -> usage ()
+;;
 
 (* lancement de ce main *)
 let () = main ()
